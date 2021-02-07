44df2864119c2c5d048e955d4b93e13f51f771d0
lownotelee <lownotelee@gmail.com>
2021-02-07 12:58:20 +1100

    Initial Commit

---
+++ day25challenge/day25challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  day25challengeApp.swift
+//  day25challenge
+//
+//  Created by Lee on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct day25challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
